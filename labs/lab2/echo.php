92bcbe6b4993dbb5085e38b32372df68e7a5cd62
vadagasy <vadagasy@mail.uc.edu>
2024-02-08 23:19:27 -0500

    Adding changes for lab2

--- labs/lab2/echo.php
+++ labs/lab2/echo.php
@@ -1,4 +1,7 @@
 <?php 
-echo $_REQUEST["data"];
-
+if(empty($_REQUEST["data"])){
+exit("please enter the input filed 'data'");
+} 
+$input=htmlentities($_REQUEST["data"]);
+echo("The input from the request is <strong>" .$input. "</strong>.<br>");
 ?>
