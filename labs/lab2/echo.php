40be476d27e30ae12960cdad5be887b7a913a461
vadagasy <vadagasy@mail.uc.edu>
2024-03-20 23:11:00 -0400

    lab2

--- labs/lab2/echo.php
+++ labs/lab2/echo.php
@@ -1,7 +1,3 @@
 <?php 
-if(empty($_REQUEST["data"])){
-exit("please enter the input filed 'data'");
-} 
-$input=htmlentities($_REQUEST["data"]);
-echo("The input from the request is <strong>" .$input. "</strong>.<br>");
+ 	echo $_REQUEST["data"];
 ?>
--- labs/lab2/email.js
+++ labs/lab2/email.js
@@ -4,8 +4,8 @@ if (shown){
   document.getElementById('email').innerHTML = "Show my email";
 shown = false;
 }else{
-var myemail = "<a href='mailto:karrapy"+ "@" +
-"karrapy.edu'>karrapy" + "@" + "karrapy@mail.uc.edu</a>" ;
+var myemail = "<a href='mailto:vadagasy"+ "@" +
+"uc.edu'>vadagasy" + "@" + "vadagasy@mail.uc.edu</a>" ;
 document.getElementById('email').innerHTML= myemail;
 shown = true;
 }
