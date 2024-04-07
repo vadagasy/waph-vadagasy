e6d75b9d9cb81bd4149a820752c2d9fc95f54cc8
vadagasy <157663434+vadagasy@users.noreply.github.com>
2024-04-07 19:35:58 -0400

    Update index.php

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,29 +1,30 @@
- <?php
+<?php
+	session_set_cookie_params(15*60,"/","vadagasy.waph.io",TRUE,TRUE);
 	session_start(); 
 	if (isset($_POST["username"]) and isset($_POST["password"])){  
 		if (checklogin_mysql($_POST["username"],$_POST["password"])) {
-			$_SESSION['authenticated'] = TRUE;
-			$_SESSION['username'] = $_POST["username"];	
-			//$_SESSION["browser"] = $_SERVER["HTTP_USER_AGENT"];
+			$_SESSION["authenticated"] = TRUE;
+			$_SESSION["username"] = $_POST["username"];	
+			$_SESSION["browser"] = $_SERVER["HTTP_USER_AGENT"];
 		}else{
 			session_destroy();
 			echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
 			die();
 		}
 	}
-	if (!$_SESSION['authenticated'] or $_SESSION['authenticated'] != TRUE)
+	if (!$_SESSION["authenticated"] or $_SESSION["authenticated"] != TRUE)
 	{
 		session_destroy();
 		echo "<script>alert('You have not login.PLease login first');</script>";
 		header("Refresh:0; url=form.php");
 		die();
 	}
-	/*if ( $_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"]){
+	if ( $_SESSION["browser"] != $_SERVER["HTTP_USER_AGENT"]){
 		session_destroy();
 		echo "<script>alert('Sesssion Hijacking is detected!');</script>";
 		header("Refresh:0; url=form.php");
 		die();
-	}*/
+	}
 	/*function checklogin($username, $password) {
 		$account = array("admin","1234");
 		if (($username== $account[0]) and ($password == $account[1])) 
@@ -37,27 +38,20 @@
 					printf("Datebase connection failed: %s\n", $mysql->connect_error);
 					exit();
 					}
-	$sql = "SELECT  * FROM users  WHERE username='" . $username . "' ";
+				/*$sql = "SELECT  * FROM users  WHERE username='" . $username . "' ";
 					$sql=$sql . " AND password = md5('" . $password . "')";
-			$stmt = $mysqli->prepare($sql);
-		$stmt->bind_param("ss", $username,$password);
-		$stmt->execute();
-		$result=$stmt->get_result();
-		if($result->num_rows ==1) return TRUE;
-		return FALSE;
-		/*echo "DEBUG>sql=$sql";
+		echo "DEBUG>sql=$sql";
 	 	  //return TRUE;
 		$result = $mysqli->query($sql);
 		*/
 
-		/*$prepared_sql = "SELECT * FROM users WHERE username= ? " . " AND password=md5(?);";
+		$prepared_sql = "SELECT * FROM users WHERE username= ? " . " AND password=md5(?);";
 		$stmt = $mysqli->prepare($prepared_sql);
 		$stmt->bind_param("ss", $username,$password);
 		$stmt->execute();
 		$result=$stmt->get_result();
-		if($result->num_rows ==1)ss
+		if($result->num_rows ==1)
 			return TRUE;
-			*/
   	}
 ?>
   	<h2> Welcome <?php echo htmlentities($_SESSION['username']); ?> !</h2>
