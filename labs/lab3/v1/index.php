4854344a3d94b9d39213f0ef86a7f3c967eeb1cc
vadagasy <vadagasy@mail.uc.edu>
2024-04-08 21:57:04 -0400

    lab3 done

--- labs/lab3/v1/index.php
+++ labs/lab3/v1/index.php
@@ -8,13 +8,13 @@
 		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
 		die();
 	}
-	function checklogin($username, $password) {
+	/*function checklogin($username, $password) {
 		$account = array("admin","1234");
 		if (($username== $account[0]) and ($password == $account[1])) 
 		  return TRUE;
 		else 
 		  return FALSE;
-  	}
+  	}*/
   	function checklogin_mysql($username, $password) {
 		$mysqli = new mysqli('localhost','vadagas' /*DB username*/,'Pa$$w0rd' /*DB password*/,'waph3' /*DB name*/);
 				if ($mysqli->connect_errno){
@@ -23,16 +23,16 @@
 					}
 				$sql = "SELECT  * FROM users  WHERE username='" . $username . "' ";
 					$sql=$sql . " AND password = md5('" . $password . "')"; 
-		//echo "DEBUG>sql=$sql";
-	 	  //return TRUE;
+		echo "DEBUG>sql=$sql";
+	 	return TRUE;
 		$result = $mysqli->query($sql);
 		
 
-		/*$prepared_sql = "SELECT * FROM users WHERE username= ? " . " AND password=md5(?);";
+		/*prepared_sql = "SELECT * FROM users WHERE username= ? " . " AND password=md5(?);";
 		$stmt = $mysqli->prepare($prepared_sql);
 		$stmt->bind_param("ss", $username,$password);
 		$stmt->execute();*/
-		//$result=$stmt->get_result();
+		result=$stmt->get_result();
 		if($result->num_rows ==1)
 			return TRUE;
 			return FALSE;
